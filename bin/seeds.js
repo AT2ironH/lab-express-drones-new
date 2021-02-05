61ae28bacd813cc4201d563c60fb63571a0a7e38
Aleksandar <aleksandar.m.trifunovic@gmail.com>
2021-02-05 20:11:35 +0100

    5 itterations done, only styling remaining

--- bin/seeds.js
+++ bin/seeds.js
@@ -1,1 +1,26 @@
 // Iteration #1
+const mongoose = require("mongoose");
+
+// first check if our db is connected
+require("../configs/db.config.js");
+
+// require the model
+
+let DroneModel = require("../models/Drone.model.js");
+
+const drones = [
+  { name: "Creeper XL 500", propellers: 3, maxSpeed: 12 },
+  { name: "Racer 57", propellers: 4, maxSpeed: 20 },
+  { name: "Courier 3000i", propellers: 6, maxSpeed: 18 },
+];
+
+  DroneModel.create(drones)
+    .then(() => {
+    
+      console.log("Seed complete");
+        mongoose.connection.close();
+      
+    })
+    .catch(() => {
+      console.log("something went wrong creating");
+    })
--- routes/drones.js
+++ routes/drones.js
@@ -1,37 +1,104 @@
 const express = require('express');
 
 // require the Drone model here
+let DroneModel = require("../models/Drone.model.js");
 
 const router = express.Router();
 
 router.get('/drones', (req, res, next) => {
   // Iteration #2: List the drones
   // ... your code here
+  DroneModel.find()
+    .then((dronesFromMongo) => {
+      // please create this page in your views folder
+      
+      res.render(`drones/list.hbs`, { dronesFromMongo });
+      
+    })
+    .catch(() => {
+      console.log("Something went wrong while finding");
+    });
+    
 });
 
-router.get('/drones/create', (req, res, next) => {
+router.get("/drones/create", (req, res, next) => {
   // Iteration #3: Add a new drone
   // ... your code here
-});
+  res.render(`drones/create-form.hbs`)
+})
 
-router.post('/drones/create', (req, res, next) => {
+router.post("/drones/create", (req, res, next) => {
   // Iteration #3: Add a new drone
   // ... your code here
+
+  const { myDrone, propellers, maxSpeed } = req.body;
+  let myNewDrone = {
+    name: myDrone,
+    propellers: propellers,
+    maxSpeed: maxSpeed,
+  };
+
+  DroneModel.create(myNewDrone)
+    .then(() => {
+      res.redirect("/drones");
+    })
+    .catch(() => {
+      console.log("something went wrong creating");
+    });
 });
 
 router.get('/drones/:id/edit', (req, res, next) => {
   // Iteration #4: Update the drone
   // ... your code here
-});
+    // grab the todo id from the url
+    let id = req.params.id;
+
+    DroneModel.findById(id)
+      .then((drones) => {
+        res.render("drones/update-form.hbs", { drones });
+      })
+      .catch(() => {
+        console.log("Something went wrong while getting a drone");
+      });
+  });
+
 
 router.post('/drones/:id/edit', (req, res, next) => {
   // Iteration #4: Update the drone
   // ... your code here
-});
+    let id = req.params.id
+    const { myDrone, propellers, maxSpeed } = req.body;
+    let myUpdatedDrone = {
+      name: myDrone,
+      propellers: propellers,
+      maxSpeed: maxSpeed,
+    };
 
-router.post('/drones/:id/delete', (req, res, next) => {
+    DroneModel.findByIdAndUpdate(id, myUpdatedDrone)
+      .then(() => {
+        res.redirect("/drones");
+      })
+      .catch(() => {
+        console.log("something went wrong creating");
+      });
+  });
+
+
+router.get('/drones/:id/delete', (req, res, next) => {
   // Iteration #5: Delete the drone
   // ... your code here
-});
+    let id = req.params.id
+    DroneModel.findByIdAndDelete(id)
+      .then(() => {
+        // when deleted successfully
+        // redirect the user to the /todos page
+
+        res.redirect("/drones")
+      })
+      .catch(() => {
+        console.log("Delete failed!")
+      });
+  });
+
 
 module.exports = router;
